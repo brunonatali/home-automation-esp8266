e12c7ee6381ba19fb83cb91734ecbc5ee79ffbff
Bruno Natali <b010010010n@gmail.com>
2021-05-27 10:45:02 -0300

    Added header and license

--- main/TouchButtonModule.h
+++ main/TouchButtonModule.h
@@ -1,26 +1,25 @@
-/*
-General bounce effect for IO
-Copyright (c) 2021 Bruno Natali - b010010010n@gmail.com
-
-License (MIT license):
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
+/**
+ * Module to deal with touch events
+ * Copyright (c) 2021 Bruno Natali - b010010010n@gmail.com
+ * 
+ * License (MIT license):
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
 */
 
 #ifndef TouchButtonModule_h
@@ -50,7 +49,22 @@ class TouchButtonModule
     bool _buttonHolded = false;
     
   public:
+    /**
+     * TouchButtonModule constructor
+     * 
+     * @param pin IO number 
+     * @param buttonNumber System button number (generically a index)
+     * @param buttonDefLevel Tell if button is normally HIGH or LOW state
+     * @param buttonHoldTimeOut Time in miliseconds button must be holded to set it`s hold
+     * @param buttonHoldPeriod Time in miliseconds that button still in hold mode
+     * 
+     * @return TouchButtonModule
+    */
     TouchButtonModule(int pin, int buttonNumber, bool buttonDefLevel, int buttonHoldTimeOut, int buttonHoldPeriod);
+
+    /**
+     * Destructor calls disable()
+    */
     ~TouchButtonModule(void);
     void disable(void);
     void enable(void);
